e789ce8278d29ecd97c9ba284fe99e6bc76bfd1b
Alex <alex@q-83.com>
2021-05-31 23:45:03 +1000

    update the test file

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -3,6 +3,7 @@ import TodoItem from '../Todo_item.vue'
 
 describe('Todo Item', () => {
   it('Todo Item should ', () => {
+    // REVIEW <学习> GENERATE A COMPONENT
     const todoItem = shallowMount(TodoItem, {
       propsData: {
         todoItem: 'test',
@@ -12,15 +13,14 @@ describe('Todo Item', () => {
       }
     })
 
-    expect(todoItem).toBeTruthy()
-
+    expect(todoItem.exists()).toBeTruthy()
     const deleteBtn = todoItem.find('.delete-btn')
     deleteBtn.trigger('click')
     expect(todoItem.emitted('deleteTodo')).toBeTruthy()
 
     const doneBtn = todoItem.find('.done-btn')
+    // REVIEW <学习> trigger button click action
     doneBtn.trigger('click')
-    console.log('helo world');
     expect(todoItem.emitted('done')).toBeTruthy()
   })
 
